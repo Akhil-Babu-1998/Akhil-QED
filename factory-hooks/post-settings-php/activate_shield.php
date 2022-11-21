13bee88be6140545eb44559284d913d15908cf10
akhil <akhilbabui1998@gmail.com>
2022-11-21 10:25:17 +0530

    QED-Onboarding: Local site with ddev & blt (Stabdard profile)and factory hooks.

---
+++ factory-hooks/post-settings-php/activate_shield.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+*  Enable http authentication.
+*/
+$config['shield.settings']['shield_enable'] = TRUE;
+$config['shield.settings']['credentials']['shield']['user'] = 'username';
+$config['shield.settings']['credentials']['shield']['pass'] = 'password';
---
+++ factory-hooks/post-settings-php/fast_404.php
@@ -0,0 +1,7 @@
+<?php
+/**
+*  Enable fast 404 pages.
+*/
+
+$config['system.performance']['fast_404']['paths'] = '/\.(?:txt|png|gif|jpe?g|css|js|ico|swf|flv|cgi|bat|pl|dll|exe|asp)$/i';
+$config['system.performance']['fast_404']['html'] = '<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL "@path" was not found on this server.</p></body></html>';
